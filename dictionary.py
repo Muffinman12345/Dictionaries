7b97552cde191360164cde78b4a3cb2b08e83d7f
Nicholas W <77017235+Muffinman12345@users.noreply.github.com>
2022-01-15 08:16:14 -0500

    Create dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,15 @@
+#This is the SYNTAX for creating a dictionary:
+a_Mustang = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+#You can print your dictonary:
+
+print(a_Mustang)
+
+#You can print a specific item in your dictonary like this:
+
+print(a_Mustang[brand])
+
